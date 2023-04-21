372b71f9c5f8615397917172457f9c11488b971d
Debashis Panigrahi <dpdebashis24@gmail.com>
2023-04-21 19:27:27 +0530

    Initial commit

---
+++ FileUploadAPI/Controllers/UploadController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FileUploadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult FileUpload(InputModel inputModel)
+        {
+            try
+            {
+                var uploadModel = JsonConvert.DeserializeObject<UploadModel>(inputModel.FileData);
+                Uploading(uploadModel);
+                return Ok("Uploaded");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        private void Uploading(UploadModel uploadModel)
+        {
+            string filePath = Path.Combine(@"/Users/debashispanigrahi/Documents",uploadModel.FileName); // example file path
+
+            System.IO.File.WriteAllBytes(filePath, uploadModel.FileBytes);
+
+        }
+    }
+}
---
+++ FileUploadAPI/UploadModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace FileUploadAPI
+{
+	public class InputModel
+	{
+		public string FileData { get; set; }
+	}
+
+    public class UploadModel
+	{
+		public byte[] FileBytes { get; set; }
+		public string FileName { get; set; }
+	}
+}
+
